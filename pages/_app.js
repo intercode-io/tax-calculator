556431f9440e81e526eb06853152a5835f0e16b4
Назар <pagor.n2003@gmail.com>
2022-07-06 13:13:23 +0300

    add tag

--- pages/_app.js
+++ pages/_app.js
@@ -46,7 +46,7 @@ function MyApp({Component, pageProps}) {
           `,
                 }}
             />
-            <Script>
+            <Script id="google_tag" strategy="lazyOnload">
                 {` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
