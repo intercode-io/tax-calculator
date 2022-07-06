4049bcf69aa27efb3488b22b4642bc4cec5da7e1
Назар <pagor.n2003@gmail.com>
2022-07-06 16:33:22 +0300

    add service worker

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,14 +1,14 @@
-const { SitemapStream, streamToPromise } = require("sitemap");
-const { Readable } = require("stream");
+const {SitemapStream, streamToPromise} = require("sitemap");
+const {Readable} = require("stream");
 
 export default async (req, res) => {
     // An array with your links
     const links = [
-        { url: "/", changefreq: "daily", priority: 0.3 },
+        {url: "/", changefreq: "daily", priority: 0.3},
     ];
 
     // Create a stream to write to
-    const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
+    const stream = new SitemapStream({hostname: `https://${req.headers.host}`});
 
     res.writeHead(200, {
         "Content-Type": "application/xml",
