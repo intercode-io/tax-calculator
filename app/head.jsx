1bd19a702deac431429b892a2797855decd1549e
Nazar <nazarpahor@intercode.io>
2023-02-17 11:34:42 +0200

    add new SEO

--- app/head.jsx
+++ app/head.jsx
@@ -1,15 +1,38 @@
 export default function Head() {
     return (
         <>
-            <title>Калькулятор розмитнення</title>
+            <title>Калькулятор розмитнення авто</title>
             <link rel="manifest" href="/manifest.json"/>
-            <link rel="apple-touch-icon" href="/icon.png"></link>
+            <link rel="icon" href="/favicon.ico"></link>
+            <link rel="apple-touch-icon" href="/favicon.ico"/>
+            <meta name="apple-mobile-web-app-title" content="Калькулятор розмитнення авто"/>
+            <meta name="apple-mobile-web-app-capable" content="yes"/>
+            <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
+            <meta name="application-name" content={'Калькулятор розмитнення авто'}/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="theme-color" content="#fff"/>
             <meta name="description"
-                  content={'Калькулятор розмитнення - розрахуйте вартість розмитнення авто в Україні.' +
+                  content={'Калькулятор розмитнення авто - розрахуйте вартість розмитнення авто в Україні.' +
                       ' Ми використовуємо найсвіжіші правила та формули згідно додатку ДІЯ.' +
-                      ' Зробіть правильний вибір для розрахунку вартості розмитнення та митних платежів у цьому авто калькуляторі.'}/>
+                      ' Зробіть правильний вибір для розрахунку вартості розмитнення авто та митних платежів у нашому калькуляторі розмитенення.'}/>
             <meta name="google-site-verification" content="_LjWHvblCsANKdhqYPY95TPMlEnA1L5_UBgh59GU1MQ"/>
+            <meta name="twitter:card" content="summary"/>
+            <meta name="twitter:url" content="https://leasing-motors.com/"/>
+            <meta name="twitter:title" content="Калькулятор розмитнення авто"/>
+            <meta name="twitter:description"
+                  content={'Калькулятор розмитнення авто - розрахуйте вартість розмитнення авто в Україні.' +
+                      ' Ми використовуємо найсвіжіші правила та формули згідно додатку ДІЯ.' +
+                      ' Зробіть правильний вибір для розрахунку вартості розмитнення авто та митних платежів у нашому калькуляторі розмитенення.'}/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content="Калькулятор розмитнення авто"/>
+            <meta property="og:description"
+                  content={'Калькулятор розмитнення авто - розрахуйте вартість розмитнення авто в Україні.' +
+                      ' Ми використовуємо найсвіжіші правила та формули згідно додатку ДІЯ.' +
+                      ' Зробіть правильний вибір для розрахунку вартості розмитнення авто та митних платежів у нашому калькуляторі розмитенення.'}/>
+            <meta property="og:site_name" content="Калькулятор розмитнення авто"/>
+            <meta name="msapplication-tap-highlight" content="no"/>
+            <meta property="og:url" content="https://leasing-motors.com/"/>
+            <link rel="canonical" href='https://leasing-motors.com/'/>
         </>
     )
 }
